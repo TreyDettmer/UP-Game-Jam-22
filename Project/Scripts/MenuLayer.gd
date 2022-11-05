cff3a4282fadd5c7ec62d96702c13a8c2d726136
yamesboy <89492659+hurstj22@users.noreply.github.com>
2022-11-05 13:53:05 -0700

    connected the high score signal

--- Project/Scripts/MenuLayer.gd
+++ Project/Scripts/MenuLayer.gd
@@ -4,7 +4,7 @@ onready var gameOverScore = get_node("GameOverMenu/Score")
 #onready var gameOverHighScore = get_node("GameOverMenu/HighScoreEndGame")
 
 var score
-#var highScore
+var highScore
 signal endGame(score)
 signal restartGame(score)
 
@@ -39,3 +39,9 @@ func _on_RestartButton_pressed():
 
 func _on_OptionsMenu_endGameFromMenu():
 	_on_QuitButton_pressed()
+
+
+func _on_Main_updateHighScore(prevHighScore):
+	$HighScoreLabel.set_text("Prev Best: " + str(prevHighScore))
+	highScore = prevHighScore
+

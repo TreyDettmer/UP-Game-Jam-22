0706db0e046da56db7f50f95eaaa4e7661101e84
yamesboy <89492659+hurstj22@users.noreply.github.com>
2022-11-05 13:10:12 -0700

    EndMenu

--- Project/Scripts/Main.gd
+++ Project/Scripts/Main.gd
@@ -4,6 +4,8 @@ signal updateScore(newScore)
 signal updateOrganisms(numOrgs)
 signal updateObstacles(numObs)
 
+const SAVE_FILE_PATH = "user://savedata.save"
+
 export var currentScore = 0
 var highScore = 0
 
@@ -19,8 +21,31 @@ func _ready():
 	emit_signal("updateScore", currentScore)
 	emit_signal("updateObstacles", initialObstacles)
 	emit_signal("updateOrganisms", initialOrganisms)
+	load_highScore()
 	
 func _on_ScoreBucket_organism_scored():
 	currentScore+=1
 	emit_signal("updateScore", currentScore)
 	print(currentScore)
+	
+func gameOver():
+	#call the end screen
+	
+	if currentScore > highScore:
+		highScore = currentScore
+		save_highScore()
+
+func save_highScore():
+	var save_data = File.new()
+	save_data.open(SAVE_FILE_PATH, File.WRITE)
+	save_data.store_var(highScore)
+	save_data.close()
+	
+
+func load_highScore():
+	var save_data = File.new()
+	#must check if it exists first
+	if save_data.file_exists(SAVE_FILE_PATH):
+		save_data.open(SAVE_FILE_PATH, File.READ)
+		highScore = save_data.get_var() #reads first variable
+		save_data.close()
